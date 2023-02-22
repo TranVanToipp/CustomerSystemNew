69527843ea1b724967ba05fcc759e68abdd665bd
Trần Văn Tới <94825061+TranVanToipp@users.noreply.github.com>
2023-02-22 17:00:39 +0700

    commit lần 2

--- CustomerSystem_Servlet/src/main/java/entyti/Account.java
+++ CustomerSystem_Servlet/src/main/java/entyti/Account.java
@@ -1,29 +1,47 @@
-package entyti;
+/**
+ * @(#)T001Dto.java 01-00 2021/07/16
+ *
+ * Copyright(C) 2021 by FUJINET CO., LTD.
+ *
+ * Last_Update 2021/07/16
+ * Version 1.00.
+ */
+package fjs.cs.dto;
 
-public class Account {
-	public String UserName,PassWord;
-	public Account() {
-	}
-	
-	public Account(String userName, String passWord) {
-		UserName = userName;
-		PassWord = passWord;
+/**
+ * T001Dto
+ * 
+ * @version 1.00
+ * @since 1.00
+ * @author Long-PH
+ * 
+ */
+public class T001Dto {
+	public String USERID, PASSWORD;
+
+	public T001Dto() {
+
 	}
 
-	public String getUserName() {
-		return UserName;
+	public T001Dto(String uSERID, String pASSWORD) {
+		super();
+		USERID = uSERID;
+		PASSWORD = pASSWORD;
 	}
 
-	public void setUserName(String userName) {
-		UserName = userName;
+	public String getUSERID() {
+		return USERID;
 	}
 
-	public String getPassWord() {
-		return PassWord;
+	public void setUSERID(String uSERID) {
+		USERID = uSERID;
 	}
 
-	public void setPassWord(String passWord) {
-		PassWord = passWord;
+	public String getPASSWORD() {
+		return PASSWORD;
 	}
-	
+
+	public void setPASSWORD(String pASSWORD) {
+		PASSWORD = pASSWORD;
+	}
 }
